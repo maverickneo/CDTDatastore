59a74761884cfde30d53722d2021622654718382
Enrique de la Torre Fernandez <enriqued@uk.ibm.com>
2015-04-23 11:09:07 +0100

    Add copyright to new files

--- Classes/common/Encryption/Keychain/CDTEncryptionKeychainData.m
+++ Classes/common/Encryption/Keychain/CDTEncryptionKeychainData.m
@@ -1,9 +1,9 @@
 //
 //  CDTEncryptionKeychainData.m
-//
+//  CloudantSync
 //
 //  Created by Enrique de la Torre Fernandez on 12/04/2015.
-//
+//  Copyright (c) 2015 IBM Cloudant. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 //  except in compliance with the License. You may obtain a copy of the License at
